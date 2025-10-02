7683bb5e2bf98a40c4776d3dff755d8349a5e2ff
bianders <phat@biden.co.uk>
2025-10-02 10:52:00 -0500

    merge

--- rot13.py
+++
@@ -1,8 +0,0 @@
-def rot13(text):
-    # Create the translation table
-    input_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-    output_chars = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
-    trans_table = str.maketrans(input_chars, output_chars)
-
-    # Apply the translation
-    return text.translate(trans_table)
